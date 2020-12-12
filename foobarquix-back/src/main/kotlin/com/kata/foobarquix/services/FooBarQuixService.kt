8d6268eb2a5380b189de2c739a0ee92eac615d41
manelby <manelbenyahia1@gmail.com>
2020-12-12 20:50:20 +0100

    back-end service fooBarQuix and feature ui form input number

--- foobarquix-back/src/main/kotlin/com/kata/foobarquix/services/FooBarQuixService.kt
+++ foobarquix-back/src/main/kotlin/com/kata/foobarquix/services/FooBarQuixService.kt
@@ -1,12 +1,39 @@
 package com.kata.foobarquix.services
 
 import org.springframework.stereotype.Component
+import java.util.HashMap
+import java.util.stream.Collectors
 
 @Component
 class FooBarQuixService {
+    val THREE = 3
+    val FIVE = 5
+    val SEVEN = 7
 
     fun convertNumber(inputNumber: Int): String {
-        return ""
+        val fooBarQixMap: MutableMap<Int, String> = HashMap()
+        fooBarQixMap[THREE] = "Foo"
+        fooBarQixMap[FIVE] = "Bar"
+
+        var toReturn: String? = division(inputNumber, fooBarQixMap)
+        val stringGivenNumber: String = inputNumber.toString()
+        fooBarQixMap[SEVEN] = "Qix"
+        toReturn += applyTheContainsFooBar(fooBarQixMap, stringGivenNumber)
+        return if (toReturn!!.isEmpty()) inputNumber.toString() else toReturn
+
+    }
+
+    private fun division(givenNumber: Int, fooBarQixMap: Map<Int, String>): String? {
+        return fooBarQixMap.keys.stream()
+                .filter { number: Int -> givenNumber % number == 0 }
+                .map { key: Int -> fooBarQixMap[key] }
+                .collect(Collectors.joining())
+    }
+
+    private fun applyTheContainsFooBar(fooBarQixMap: Map<Int, String>, stringGivenNumber: String): String? {
+        return stringGivenNumber.chars()
+                .mapToObj { charUnit: Int -> fooBarQixMap[Character.getNumericValue(charUnit)] ?: "" }
+                .collect(Collectors.joining())
     }
 
 }
--- foobarquix-ui/src/app/foo-bar-quix-form/foo-bar-quix-form.component.ts
+++ foobarquix-ui/src/app/foo-bar-quix-form/foo-bar-quix-form.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
@@ -6,16 +7,27 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './foo-bar-quix-form.component.html'
 })
 export class FooBarQuixFormComponent implements OnInit {
+  form: FormGroup;
 
 
-  constructor() {
+  constructor(private http: HttpClient, public formBuilder: FormBuilder) {
+    this.createForm();
 
+  }
+
+  createForm() {
+    this.form = this.formBuilder.group({
+      inputNumber: ['', Validators.required]
+    });
   }
 
   ngOnInit(): void {
   }
 
   submitNumber(): void {
+    let formData: FormData = new FormData();
+    formData.append("inputNumber", this.form.get('inputNumber').value);
+    console.log(this.form.value);
   }
 
 }
